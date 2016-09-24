62f1800a4aae157524cb5dc25c842d65ee629b68
Saul Juarez <saulzini@gmail.com>
2016-09-23 20:54:37 -0500

    Ciclo fallido

--- bfs.py
+++ bfs.py
@@ -6,7 +6,7 @@ end_state = []
 visited = []
 forExploring=[]
 max_stack = []
-
+cost = 0
 
 class Node():
     def __init__(self, state, parent, action, path_cost):
@@ -39,6 +39,8 @@ def readingInput():
             initial_state = initial_state.split(';')
             for i, element in enumerate(initial_state):
                 initial_state[i] = element.split(",")
+                if (initial_state[i] == ['']):
+                    initial_state[i].pop()
             print(initial_state)
         elif i == 3:  # reads the third line, which contains the end state
             # the following block formats the line and creates the end state data structure
@@ -63,57 +65,82 @@ def bfs():
     global end_state
     global visited 
     global forExploring
+    initial_state = Node(initial_state,None,None,0)
     forExploring.append(initial_state)
     visited.append(initial_state)
-    current_node =[]
-    while(forExploring and comparingNode( end_state,current_node ) != 0 ):#if not empty
+    current_node =Node(None,None,None,0)
+    i=0
+    while(forExploring and  current_node.state  != end_state ):#if not empty
         current_node=forExploring.pop(0)#getting first element of the list
-        print ("Current",current_node)
+        #print ("Current",current_node.state)
         creatingNode(current_node)
         
-        if (not forExploring and comparingNode( end_state,current_node ) != 0 ): #if empty
+        if (current_node.state  == end_state ): #if exito
+            print_path(current_node)
+        
+        if (not forExploring and current_node.state  != end_state): #if empty
             print("No solution found")
-                    
+        if (i==2):
+            break
+        i=i+1
         
 #method for comparing the nodes with the end node
 def comparingNode(endNode,currentNode ):
-    if(currentNode):#if not empty
+    if(currentNode.state):#if not empty
         for element in range(len(endNode)):
-            if (currentNode[element]): #if not empty:
+            if (currentNode.state[element]): #if not empty:
                 if (not endNode[element] ): #if it is empty
-                    currentNode[element] = [] #convert the listo to blank
+                    currentNode.state[element] = [] #convert the listo to blank
         return cmp(endNode , currentNode) #return the comparison
     
             
 
-def creatingNode(state):
+def creatingNode(currentNode):
     leaves_nodes=[] #variable that has the results of the nodes
     
-    for element in range(len(state)): 
-        for listNode in range(len(state)):
-            list_Nodes=copy.deepcopy(state) #copying the state
-            if (list_Nodes[element]): #if not empty
+    for element in range(len(currentNode.state)): 
+        for listNode in range(len(currentNode.state)):
+            list_Nodes=copy.deepcopy(currentNode.state) #copying the state
+            if (list_Nodes[element]  ): #if not empty
                 
-                if (list_Nodes[element]): #in case the spot is not empty
-                   
-                    list_Nodes[element].pop(0)#remove the element from the list                       
-                
+                list_Nodes[element].pop(0)#remove the element from the list 
+                                
                 
                 if (element != listNode and len(list_Nodes[listNode]) < max_stack ):  #not moving to the same place and the lenght is less or equal to stack
-                                                        
-                    list_Nodes[listNode].append(state[element][0]) #adding the new element to the assigned space
-                    leaves_nodes.append(list_Nodes) #Saving Result
-                    
+                    list_Nodes[listNode].append(currentNode.state[element][0]) #adding the new element to the assigned space
+                    newCost = 1 + abs(element - listNode) + cost
+                    newNode= Node(list_Nodes,currentNode,[element,listNode],newCost)
+                    print ("--------Final", newNode.state)
+                    leaves_nodes.append(newNode) #Saving Result
+        
     comparingLists(visited,leaves_nodes)
+    
 #method for comparing lists and adding to visited and for exploring    
 def comparingLists(listVisited , listNewNodes):
         global visited
-        for nodeVisited in range(len(listVisited)):
-            for nodeNew in range(len(listNewNodes)):
-                if (cmp(listVisited[nodeVisited], listNewNodes[nodeNew]) != 0): #if the nodes are not in the visited list
+        global forExploring
+       
+        for nodeVisited in range(0,len(listVisited)):
+            for nodeNew in range(0,len(listNewNodes)):
+                
+                
+                if (cmp(listVisited[nodeVisited].state , listNewNodes[nodeNew].state ) != 0): #if the nodes are not in the visited list
+                    print ("ListaC", listNewNodes[nodeNew].state )
                     forExploring.append(listNewNodes[nodeNew]) #adding new nodes to forExploring
                     visited.append(listNewNodes[nodeNew]) #adding new nodes to visited
-        
+      
+        print("FINAL ITERACION")            
+                
+def print_path(node):
+    actual = node
+    steps = []
+    if(node != None):
+        print(node.path_cost)
+    while(actual != None):
+        if(actual.parent != None):
+            steps.append(actual.action)
+        actual = actual.parent
+    print(steps)
                         
 readingInput()
 bfs()
